ad233c8cd695bc29b5180184e663ffe4b5752908
Wubbo Boiten <97003968@st.deltion.nl>
2026-04-14 12:14:01 +0200

    First commit

--- WeConvert/ContentView.swift
+++ WeConvert/ContentView.swift
@@ -9,15 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form{
+                Section(header: Text("Pick a format")){
+                    
+                }
+            }
+            .navigationTitle("WeConvert")
         }
-        .padding()
+        
     }
 }
+    
 
 #Preview {
     ContentView()
