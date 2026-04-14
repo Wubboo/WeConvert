f5cd31054898dc57f745e3a92c98389fb93655f7
Wubbo Boiten <97003968@st.deltion.nl>
2026-04-14 12:05:14 +0200

    Initial Commit

---
+++ WeConvert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeConvert
+//
+//  Created by Wubbo Boiten on 14/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by Wubbo Boiten on 14/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
